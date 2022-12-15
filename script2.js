bc7e03585030a3260054495e6cf5b12f4042fb37
Balasubiramani G <103410862+Balaguru89@users.noreply.github.com>
2022-12-16 03:10:04 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  clickable:"true",
+  centeredSlides: true,
+  slidesPerView: "3",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:"true"
+  },
+});
